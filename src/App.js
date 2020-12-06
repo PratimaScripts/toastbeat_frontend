eb9f51a964d903296b3abb1faaca69646f6215ae
pratimagautam977 <astute.yard@gmail.com>
2020-12-06 12:05:08 +0545

    Updated with the Auth and Proteected rRoutes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import React, {useState} from "react";
+
+import {BrowserRouter as Router, Switch, Redirect, Route} from "react-router-dom";
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Navigation from './pages/Navigation';
+import Menu from './pages/Menu';
 
 function App() {
+
+  const [loggedIn, setLoggedIn] = useState(localStorage.getItem("token")? true: false);
+
+  const PrivateRoute = ({ component, ...rest }) => {
+    const isAuthed = localStorage.getItem('token');
+    console.log(isAuthed, 'dddddddddddddddddddd')
+    return (
+      <Route {...rest} exact
+        render = {(props) => (
+          isAuthed ? (
+            <div>
+              {React.createElement(component, props)}
+            </div>
+          ) :
+          (
+            <Redirect
+              to={{
+                pathname: '/login',
+                state: { from: props.location }
+              }}
+            />
+          )
+        )}
+      />
+    )
+  }
+  
   return (
       <Router>    
-            <Navigation />
+            <Navigation isAuth={loggedIn} setLoggedIn={setLoggedIn}/>
             <Switch>
               <Route exact path="/" component={Dashboard}/>
-              <Route path="/login" component={Login}/>
+              <Route path="/login" setLoggedIn={setLoggedIn} component={Login}/>
               <Route path="/register" component={Register}/>
+              
+              <PrivateRoute path="/menu/:id" component={Menu} />
             </Switch>
           
           {/* Footer Section goes here */}     
